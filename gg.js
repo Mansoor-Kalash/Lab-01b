826b1270dbc838db3f77e72eb0a5be0c27890c3c
MansoorKalash <>
2021-06-20 19:03:27 +0000

    sdd

---
+++ gg.js
@@ -0,0 +1,12 @@
+// let typeCar = prompt("What type of car you like\" Sport, Saloon car,..etc\"?")
+// console.log(typeCar);
+// alert ("thats nice");
+// let colore = prompt("whate the color of youre car you like?");
+// console.log(colore);
+// alert ("thats nice for me i choose black");
+// let speed = prompt("you love the speed?");
+// console.log(speed);
+// alert ("oh");
+// let dreem = prompt("what is the car dreem for you?");
+// console.log(dreem);
+// alert ("I like it");
